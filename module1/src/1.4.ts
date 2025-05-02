b587da45739a05f148f3bfe7d75a6d55858435ef
Sohelrana2815 <sohel152302@gmail.com>
2025-05-02 18:17:52 +0600

    learning about tuple

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -14,3 +14,28 @@ let x: undefined = undefined;
 
 // null
 let y: null = null;
+
+// Any type
+
+let d: number;
+d = 123;
+// d = "hello";
+// d = true;
+
+// Array
+
+let friends: string[] = ["jakir", "sohel", "rana"];
+// friends.push(7);
+let eligibleNumberLists: number[] = [1, 2, 3, 4, 5, 6];
+
+// eligibleNumberLists.push("hello");
+
+// tuple --> array --> order --> type of values
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [54, "sohel", true];
+
+ageName[0] = 60; // first it expect number 2nd string and 3rd it expect boolean
+
+
