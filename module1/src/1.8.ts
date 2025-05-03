46d07611cc2f4bc8c04b19fe0f2ccc6ca8767b29
Sohelrana2815 <sohel152302@gmail.com>
2025-05-03 16:34:02 +0600

    array destructuring

---
+++ module1/src/1.8.ts
@@ -0,0 +1,37 @@
+// Destructuring
+
+{
+  // Object destructuring
+
+  const user = {
+    id: 727,
+    name: {
+      firstName: "Sohel",
+      middleName: "Rana",
+      lastName: "Moon",
+    },
+
+    contactNo: "0190000000",
+    address: "Uganda",
+  };
+
+  // Destructuring
+
+  const {
+    contactNo,
+    name: { middleName },
+  } = user;
+
+  // Array destructuring
+
+  const myFriends = ["chandler", "joey", "ross", "rachel", "monica", "phoebe"];
+
+  const [, , bestFriend, ...rest] = myFriends;
+
+
+
+
+
+
+  
+}
